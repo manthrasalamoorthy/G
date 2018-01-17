43216b0f7fec0bc745e3c5b1e74ac461b50c67ef
manthrasalamoorthy <30748252+manthrasalamoorthy@users.noreply.github.com>
2018-01-17 16:09:53 +0530

    armsintl.c

---
+++ armsintl.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int num,res=0,rem,low,high,c;
+printf("enter the low and high value:");
+scanf("%d%d",&low,&high);
+c=num;
+for(num=low;num<=high;num++)
+{
+while(num!=0)
+{
+rem=num%10;
+res=res+rem*rem*rem;
+num=num/10;
+}
+if(c==num)
+{
+printf("%d",c);
+}
+}}
