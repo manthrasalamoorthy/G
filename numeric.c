4b24fcdd2ea79fe5067550cb710195d90ff4f3f3
manthrasalamoorthy <30748252+manthrasalamoorthy@users.noreply.github.com>
2018-01-22 16:59:13 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100],i;
+printf("Enter a string:");
+scanf("%s",a);
+if(a[i]>='0' && a[i] <='9')
+{
+printf("yes");
+}
+else 
+{
+printf("no");
+}
+return 0;
+}
+
