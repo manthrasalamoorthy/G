797f21fbf53f768d3871a50c8ca154ed97c5cba8
manthrasalamoorthy <30748252+manthrasalamoorthy@users.noreply.github.com>
2018-02-02 16:56:24 +0530

    Create noofnumericchar.c

---
+++ noofnumericchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,count;
+char str[100];
+printf("Enter the string:");
+scanf("%s",&str);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]>='0'&& str[i]<='9')
+{
+count++;
+}}
+printf("%d",count);
+}
