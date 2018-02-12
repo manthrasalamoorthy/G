ccf0f39d78f9d07ab39a2992e1d081e40785faab
manthrasalamoorthy <30748252+manthrasalamoorthy@users.noreply.github.com>
2018-02-12 15:47:44 +0530

    Update fibonacci60.c

--- fibonacci60.c
+++ fibonacci60.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int a=0,b=1,c,n,i;
+int a=0,b=1,c=0,n,i;
 printf("Enter the number:");
 scanf("%d",&n);
+printf(" the serious is:");
 for(i=0;i<n;i++)
 {
 a=b;
 b=c;
 c=a+b;
+printf("%d",c);
 }
-printf(" the serious is %d",c);
 return 0;
 }
