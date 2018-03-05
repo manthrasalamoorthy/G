d0be9dc836d679ab2e0de18e4474b7a856e64c17
manthrasalamoorthy <30748252+manthrasalamoorthy@users.noreply.github.com>
2018-03-05 16:53:00 +0530

    Update LCM87.c

--- LCM87.c
+++ LCM87.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+int main()
+{
+    int num1, num2, minm;
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &num1, &num2);
+    minm = (n1>n2) ? num1 : num2;
+    while(1)
+    {
+        if( minm%num1==0 && minm%num2==0 )
+        {
+            printf("The LCM of %d and %d is %d.", num1, num2,minm);
+            break;
+        }
+        ++minm;
+    }
+    return 0;
+}
