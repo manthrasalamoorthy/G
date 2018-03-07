a0b507df53a76dfb753b4f9c0ac3f26d5e2d2111
manthrasalamoorthy <30748252+manthrasalamoorthy@users.noreply.github.com>
2018-03-07 15:58:33 +0530

    Update sumofNnumber92.c

--- sumofNnumber92.c
+++ sumofNnumber92.c
@@ -1,1 +1,18 @@
-#include<
+#include<stdio.h>
+int main()
+{
+int a[10],n,i,sum=0;
+printf("Enter the array size:");
+scanf("%d",&n);
+printf("Enter the elements:");
+for(i=0;i<n;i++)
+{
+	scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+	sum=sum+a[i];
+}
+printf("%d",sum);
+return 0;
+}
