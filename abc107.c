b7ed5d76346e0d47861e47a445e2930fe249ae66
manthrasalamoorthy <30748252+manthrasalamoorthy@users.noreply.github.com>
2018-03-09 16:38:50 +0530

    Create abc107.c

---
+++ abc107.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter the A,B,C values:");
+scanf("%d%d%d",&a,&b,&c);
+printf("The answer (%d * %d)/ %d is :%d",a,b,c,(a*b)/c);
+
+
+return 0;
+}
